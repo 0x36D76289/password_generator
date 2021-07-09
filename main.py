b9ba72de156014a5d35d42dba983c9395cf43623
0x36D76289 <71098898+0x36D76289@users.noreply.github.com>
2021-07-10 01:05:31 +0200

    Copies the password to the Windows clipboard.

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
 """
 @author         catarme
-@version        0.3
-@since          01/07/2021
-@param          Code optimization.
+@version        0.4
+@since          10/07/2021
+@param          Copies the password to the Windows clipboard.
 """
 from random import randint
 from pyperclip import copy
 
-def mdp(size: int, level: int) -> str:
-    mdp = str()
-    min, max = 33, 126
-
-    if level == 1:
-        min, max = 58, 65
-    
-    if level == 2:
-        min, max = 48, 75
-
+def generatepassword(size: int, level: int) -> str:
+    """
+    Generates a "size" character password.
+    """
+    password = str()
     for i in range(size):
-        mdp += str(chr(randint(min, max)))
-    return mdp
+        password += chr(randint(33, 126))
+    return password
 
 def defsize() -> int:
+    """
+    Checks that the variable entered is usable.
+    """
     try:
         number = int(input("Number of characters in the password : "))
         if number <= 0:
@@ -31,6 +29,9 @@ def defsize() -> int:
     return number
 
 def deflevel() -> int:
+    """
+    Checks that the variable entered is usable.
+    """
     try:
         number = int(input("What level of difficulty : "))
         if number < 0:
@@ -40,4 +41,4 @@ def deflevel() -> int:
     return number
 
 if __name__ == "__main__":
-    copy(mdp(defsize(), deflevel()))
+    copy(generatepassword(defsize(), deflevel()))
